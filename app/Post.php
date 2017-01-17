64e23940967a07dd4cac87bbebb9ecfa0970ba3d
zoyo <lblzms@gmail.com>
2017-01-17 18:23:27 +0800

    1

--- app/Post.php
+++ app/Post.php
@@ -1,19 +1,166 @@
 <?php
-
 namespace App;
-
+use App\Services\Markdowner;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
-
 class Post extends Model
 {
-    /*
-     * 访问 Title 字段，进行 SEO 优化
+    protected $dates = ['updated_at'];
+    protected $fillable = [
+        'title', 'subtitle', 'content_raw', 'page_image', 'meta_description',
+        'layout', 'is_draft', 'updated_at',
+    ];
+    /**
+     * The many-to-many relationship between posts and tags.
+     *
+     * @return BelongsToMany
      */
-    public function setTitleAttribute($value){
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag', 'post_tag_pivot');
+    }
+    /**
+     * Set the title attribute and automatically the slug
+     *
+     * @param string $value
+     */
+    public function setTitleAttribute($value)
+    {
         $this->attributes['title'] = $value;
-
-        if (!$this->exists){
-            $this->attributes['slug'] = str_slug($value);
+        if (! $this->exists) {
+            $this->setUniqueSlug($value, '');
         }
+    }
+    /**
+     * Recursive routine to set a unique slug
+     *
+     * @param string $title
+     * @param mixed $extra
+     */
+    protected function setUniqueSlug($title, $extra)
+    {
+        $slug = str_slug($title.'-'.$extra);
+        if (static::whereSlug($slug)->exists()) {
+            $this->setUniqueSlug($title, $extra + 1);
+            return;
+        }
+        $this->attributes['slug'] = $slug;
+    }
+    /**
+     * Set the HTML content automatically when the raw content is set
+     *
+     * @param string $value
+     */
+    public function setContentRawAttribute($value)
+    {
+        $markdown = new Markdowner();
+        $this->attributes['content_raw'] = $value;
+        $this->attributes['content_html'] = $markdown->toHTML($value);
+    }
+    /**
+     * Sync tag relation adding new tags as needed
+     *
+     * @param array $tags
+     */
+    public function syncTags(array $tags)
+    {
+        Tag::addNeededTags($tags);
+        if (count($tags)) {
+            $this->tags()->sync(
+                Tag::whereIn('tag', $tags)->pluck('id')->all()
+            );
+            return;
+        }
+        $this->tags()->detach();
+    }
+    /**
+     * Return the date portion of published_at
+     */
+    public function getPublishDateAttribute($value)
+    {
+        return $this->updated_at->format('M-j-Y');
+    }
+    /**
+     * Return the time portion of published_at
+     */
+    public function getPublishTimeAttribute($value)
+    {
+        return $this->updated_at->format('g:i A');
+    }
+    /**
+     * Alias for content_raw
+     */
+    public function getContentAttribute($value)
+    {
+        return $this->content_raw;
+    }
+    /**
+     * Return URL to post
+     *
+     * @param Tag $tag
+     * @return string
+     */
+    public function url(Tag $tag = null)
+    {
+        $url = url('blog/'.$this->slug);
+        if ($tag) {
+            $url .= '?tag='.urlencode($tag->tag);
+        }
+        return $url;
+    }
+    /**
+     * Return array of tag links
+     *
+     * @param string $base
+     * @return array
+     */
+    public function tagLinks($base = '/blog?tag=%TAG%')
+    {
+        $tags = $this->tags()->lists('tag');
+        $return = [];
+        foreach ($tags as $tag) {
+            $url = str_replace('%TAG%', urlencode($tag), $base);
+            $return[] = '<a href="'.$url.'">'.e($tag).'</a>';
+        }
+        return $return;
+    }
+    /**
+     * Return next post after this one or null
+     *
+     * @param Tag $tag
+     * @return Post
+     */
+    public function newerPost(Tag $tag = null)
+    {
+        $query =
+            static::where('updated_at', '>', $this->updated_at)
+                ->where('updated_at', '<=', Carbon::now())
+                ->where('is_draft', 0)
+                ->orderBy('updated_at', 'asc');
+        if ($tag) {
+            $query = $query->whereHas('tags', function ($q) use ($tag) {
+                $q->where('tag', '=', $tag->tag);
+            });
+        }
+        return $query->first();
+    }
+    /**
+     * Return older post before this one or null
+     *
+     * @param Tag $tag
+     * @return Post
+     */
+    public function olderPost(Tag $tag = null)
+    {
+        $query =
+            static::where('updated_at', '<', $this->updated_at)
+                ->where('is_draft', 0)
+                ->orderBy('updated_at', 'desc');
+        if ($tag) {
+            $query = $query->whereHas('tags', function ($q) use ($tag) {
+                $q->where('tag', '=', $tag->tag);
+            });
+        }
+        return $query->first();
     }
 }
